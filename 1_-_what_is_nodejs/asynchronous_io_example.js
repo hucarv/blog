1fcae2f3c587d1140320e5c2dedc1cbcdba8451e
Humberto Menezes <humberto.menezes@gmail.com>
2015-08-29 16:25:52 -0300

    dummy files added

---
+++ 1_-_what_is_nodejs/asynchronous_io_example.js
@@ -0,0 +1,5 @@
+/*
+ * This program demonstrates that nodejs executes the I/O events
+ * asyncronously, allowing other events to be executed while it awaits for the
+ * I/O events to be completed.
+ */
